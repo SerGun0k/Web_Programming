6fd3f10cc03e69ba8a80bc3d9b580b2541fecf44
SerGun0k <113134004+SerGun0k@users.noreply.github.com>
2024-03-09 20:54:34 +0200

    Сomleted task #2

--- Lab_2/code/index.php
+++ Lab_2/code/index.php
@@ -9,3 +9,15 @@ $order .= "+7(952)812 | ";
 
 // Don't change the line below
 echo "\nYour order is: $very_bad_unclear_name.";
+echo "\n";
+$a = 52;
+echo $a;
+$float_b = 7.952812;
+echo "\n", $float_b;
+echo "\n", (19 - 7 + 1 - 1 + 3 - 2 - 1);
+
+$last_month = 1187.23;
+$this_month = 1089.98;
+echo "\n", $last_month - $this_month;
+echo "\n";
+
