aaa86af4ae5f37f2658d519860e5649a1f40959c
SerGun0k <113134004+SerGun0k@users.noreply.github.com>
2024-03-09 21:06:06 +0200

    Completed task 11, 12

--- Lab_2/code/index.php
+++ Lab_2/code/index.php
@@ -1,4 +1,5 @@
 <?php
+// 1
 /* Imagine a lot of code here */
 $very_bad_unclear_name = "15 chicken wings";
 
@@ -9,7 +10,10 @@ $order .= "+7(952)812 | ";
 
 // Don't change the line below
 echo "\nYour order is: $very_bad_unclear_name.";
+
 echo "\n";
+
+// 2
 $a = 52;
 echo $a;
 $float_b = 7.952812;
@@ -19,5 +23,16 @@ echo "\n", (19 - 7 + 1 - 1 + 3 - 2 - 1);
 $last_month = 1187.23;
 $this_month = 1089.98;
 echo "\n", $last_month - $this_month;
-echo "\n";
 
+
+// 11
+$num_languages = 4;
+$months = 11;
+$days = $months * 16;
+$days_per_language = $days / $num_languages;
+echo "\n", $days_per_language;
+
+
+// 12
+echo "\n", 8**2;
+// 13
