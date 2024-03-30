e9eae9cc5faf8545da7cbffaa88555fe5e1b4520
SerGun0k <113134004+SerGun0k@users.noreply.github.com>
2024-03-30 19:32:16 +0200

    did index_2_3.php start process_3.php

---
+++ Lab_3/code/ex_2/ex_2_3/index_2_3.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
+</head>
+<body>
+<form action="process_3.php" method="POST">
+    <label for="name">Name:</label><br>
+    <input type="text" id="name" name="name"><br>
+    <label for="age">Age:</label><br>
+    <input type="text" id="age" name="age"><br>
+    <label for="salary">Salary:</label><br>
+    <input type="text" id="salary" name="salary"><br>
+    <button type="submit">Submit</button>
+</form>
+</body>
+</html>
